6cab19c9bdbe082f4a6ff262a6e52d1fce4fd7bb
mileine <43147648+mileine@users.noreply.github.com>
2019-12-20 19:29:19 -0200

    Created Sidebar component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import Profile from "../Profile"
+import Sidebar from "../Sidebar"
 
 import * as S from './styled'
 import GlobalStyles from '../../styles/global'
@@ -17,9 +17,7 @@ const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>  
       <GlobalStyles></GlobalStyles>
-      <aside>
-        <Profile></Profile>
-      </aside>
+      <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
      </S.LayoutWrapper>
   )
