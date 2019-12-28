e119905f618d3a04642273806b14e8f2ec3b02a3
mileine <43147648+mileine@users.noreply.github.com>
2019-12-28 17:30:51 -0200

    Create home template page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,4 @@
 const path = require("path")
-// To add the slug field to each post
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -22,23 +21,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   }
 }
 
-// To create the posts pages
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
     {
-      allMarkdownRemark {
+      allMarkdownRemark (sort: {fields: frontmatter___date, order: DESC}){
         edges {
           node {
-            fields {
+            fields{
               slug
             }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
           }
         }
       }
     }
   `).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const posts = result.data.allMarkdownRemark.edges
+
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve(`./src/templates/blog-post.js`),
@@ -49,5 +57,21 @@ exports.createPages = ({ graphql, actions }) => {
         },
       })
     })
+
+    const postsPerPage = 6
+    const numPages = Math.ceil(posts.length / postsPerPage)
+  
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage ({
+        path: index === 0 ? `/`: `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1,
+        },
+      })
+    }) 
   })
 }
