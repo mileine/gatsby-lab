a4aa88a8a6488cbd45bd6aa0462031c9d0b1492c
mileine <43147648+mileine@users.noreply.github.com>
2019-12-30 18:57:58 -0200

    Create change theme method

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { Home } from "styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
@@ -8,32 +8,46 @@ import { Grid } from "styled-icons/boxicons-solid/Grid"
 
 import * as S from "./styled"
 
-const MenuBar = () => (
-  <S.MenuBarWrapper>
-    <S.MenuBarGroup>
-      <S.MenuBarLink to="/" title="Voltar para Home">
-        <S.MenuBarItem>
-          <Home></Home>
+const MenuBar = () => {
+  const [theme, setTheme] = useState("null")
+
+  const isDarkMode = theme === "dark"
+
+  useEffect (() => {
+    setTheme(window.__theme)
+    window.__onThemeChange = () => setTheme(window.__theme)
+  }, [])
+
+  return (
+    <S.MenuBarWrapper>
+      <S.MenuBarGroup>
+        <S.MenuBarLink to="/" title="Voltar para Home">
+          <S.MenuBarItem>
+            <Home></Home>
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+        <S.MenuBarLink to="/search/" title="Pesquisar">
+          <S.MenuBarItem>
+            <Search></Search>
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+      </S.MenuBarGroup>
+      <S.MenuBarGroup>
+        <S.MenuBarItem title="Mudar o tema" onClick={() => {
+          window.__setPreferredTheme(isDarkMode ? 'light': 'dark')
+        }}
+        className={theme}
+        >
+          <Light></Light>
         </S.MenuBarItem>
-      </S.MenuBarLink>
-      <S.MenuBarLink to="/search/" title="Pesquisar">
-        <S.MenuBarItem>
-          <Search></Search>
+        <S.MenuBarItem title="Mudar visualização">
+          <Grid></Grid>
         </S.MenuBarItem>
-      </S.MenuBarLink>
-    </S.MenuBarGroup>
-    <S.MenuBarGroup>
-      <S.MenuBarItem title="Mudar o tema">
-        <Light></Light>
-      </S.MenuBarItem>
-      <S.MenuBarItem title="Mudar visualização">
-        <Grid></Grid>
-      </S.MenuBarItem>
-      <S.MenuBarItem title="Ir para o Topo">
-        <Arrow></Arrow>
-      </S.MenuBarItem>
-    </S.MenuBarGroup>
-  </S.MenuBarWrapper>
-)
+        <S.MenuBarItem title="Ir para o Topo">
+          <Arrow></Arrow>
+        </S.MenuBarItem>
+      </S.MenuBarGroup>
+    </S.MenuBarWrapper>
+  )}
 
 export default MenuBar
