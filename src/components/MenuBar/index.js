194c96d7d398b1de338fcdd2761b0b44012d1fe1
mileine <43147648+mileine@users.noreply.github.com>
2020-01-22 23:22:54 -0200

    Removed 'go to top' button

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react"
-
 import { Home } from "styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
@@ -59,9 +58,6 @@ const MenuBar = () => {
           className="display"
         >
           {isListMode ? <Grid /> : <List />}
-        </S.MenuBarItem>
-        <S.MenuBarItem title="Ir para o Topo">
-          <Arrow></Arrow>
         </S.MenuBarItem>
       </S.MenuBarGroup>
     </S.MenuBarWrapper>
