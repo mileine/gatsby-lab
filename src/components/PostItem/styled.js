981b1bf65df0826c5f9ae455f357b8fc7d517dbb
mileine <43147648+mileine@users.noreply.github.com>
2020-05-02 18:10:50 -0300

    Changed tag font color

--- src/components/PostItem/styled.js
+++ src/components/PostItem/styled.js
@@ -41,7 +41,7 @@ export const PostItemTag = styled.div`
   align-items: center;
   background: ${props => props.background ? props.background : "var(--highlight)"};
   border-radius: 50%;
-  color: var(--postColor);
+  color: #fff;
   display: flex;
   font-size: .8rem;
   font-weight: 700;
