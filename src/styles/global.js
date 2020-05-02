10ced1e55ba7c48cbbd5c9c1523e6d0b5d86cd5e
mileine <43147648+mileine@users.noreply.github.com>
2020-05-02 18:15:04 -0300

    Changed highlight color

--- src/styles/global.js
+++ src/styles/global.js
@@ -58,7 +58,7 @@ const GlobalStyles = createGlobalStyle`
     --borders: #38444d;
     --texts: #8899a6;
     --postColor: #fff;
-    --highlight: #1fa1f2;
+    --highlight: #129FA1;
     --mediumBackground: #192734;
     --background: #16202c;
     --white: #fff;
@@ -69,7 +69,7 @@ const GlobalStyles = createGlobalStyle`
     --borders: #dedede;
     --postColor: #111;
     --texts: #555555;
-    --highlight: #1fa1f2;
+    --highlight: #129FA1;
     --mediumBackground: #f0f0f3;
     --background: #fff;
     --white: #fff;
