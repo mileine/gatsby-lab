c3abffedc71dec4af50e6aa7ae12af907f66330a
mileine <43147648+mileine@users.noreply.github.com>
2020-05-03 20:40:58 -0300

    Fix typo

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const AboutPage = () => (
     <SEO title="About"/>
     <MainContent>
       <h1> Sobre este blog</h1>
-      <p>Este blog foi desenvolvido durante <a href="https://www.udemy.com/share/101QMeB0oSeVtQ/">o curso de Gatsby</a> que fiz pela Udemy com o instrutor <a href="https://willianjusten.com.br/">Willian Justen.</a> Ali ele ensina passa-a-passo como criar um blog igual ao dele utilizando Gatsby. Foi bem divertido e pude aprender também um pouco sobre:</p>
+      <p>Este blog foi desenvolvido durante <a href="https://www.udemy.com/share/101QMeB0oSeVtQ/">o curso de Gatsby</a> que fiz pela Udemy com o instrutor <a href="https://willianjusten.com.br/">Willian Justen.</a> Ali ele ensina passo-a-passo como criar um blog igual ao dele utilizando Gatsby. Foi bem divertido e pude aprender também um pouco sobre:</p>
 
       <ul>
         <li>GraphQL</li>
