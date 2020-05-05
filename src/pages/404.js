f1957d2217574a2b0047d86d87f73123351ffb39
mileine <43147648+mileine@users.noreply.github.com>
2020-05-04 23:32:50 -0300

    Add 404 image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,32 @@
 import React from "react"
-
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image' 
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = () => {
+  const { notFoundImg } = 
+  useStaticQuery(
+    graphql`
+      query {
+        notFoundImg: file(relativePath: { eq: "undraw_not_found_60pq.png"}) {
+          childImageSharp {
+            fluid(maxWidth: 400) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+    `
+  )
+  return <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>Oopts... nada aqui!</p>
+    <Img
+        fluid={notFoundImg.childImageSharp.fluid}
+        alt="Oops... nada aqui!"
+        />
+    <p>Oops! Nada por aqui...</p>
   </Layout>
-)
+}
 
 export default NotFoundPage
