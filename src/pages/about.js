a8fc951edaf89b11aeb064cec1b53b05b3b28bc5
mileine <43147648+mileine@users.noreply.github.com>
2020-05-05 00:06:20 -0300

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,8 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About"/>
     <MainContent>
-      <h1> Sobre este blog</h1>
+      <h1>Sobre este blog</h1>
+      
       <p>Este blog foi desenvolvido durante <a href="https://www.udemy.com/share/101QMeB0oSeVtQ/">o curso de Gatsby</a> que fiz pela Udemy com o instrutor <a href="https://willianjusten.com.br/">Willian Justen.</a> Ali ele ensina passo-a-passo como criar um blog igual ao dele utilizando Gatsby. Foi bem divertido e pude aprender também um pouco sobre:</p>
 
       <ul>
@@ -16,12 +17,17 @@ const AboutPage = () => (
         <li>Algolia</li>
         <li>Netlify</li>
         <li>Netlify CMS</li>
-        <li>Web development best practices</li>
+        <li>ReactJS</li>
+        <li>CSS-in-JS</li>
+        <li>Boas práticas em desenvolvimento web</li>
       </ul>
 
-      <p>A partir de agora, meu objetivo é transformá-lo num blog pessoal, customizando layout, design, adicionando/removendo componentes, enquanto continuo aprendendo. =) </p>
+      <p>Meu objetivo é transformá-lo num blog pessoal, customizando layout, design, adicionando/removendo componentes, enquanto continuo aprendendo. =)</p>
+      
+      <p>A ideia inicial era criar um blog completamente do zero. Mas, depois de um tempo tentando, percebi que estava patinando bastante em conceitos muito básicos. Por isso, resolvi dar um passo para trás trabalhar em cima do projeto do curso.</p>
 
       <blockquote>"learning is hard, and it's humbling" ( <a href="https://css-tricks.com/learning-to-learn/">Learning to learn, by Sarah Drasner</a>)</blockquote>
+
      
     </MainContent>
   </Layout>
