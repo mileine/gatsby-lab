9ca8d7b83c8c66d1e30d0b38bf49d4a171adb7cc
mileine <43147648+mileine@users.noreply.github.com>
2020-06-13 19:26:27 -0300

    Add box-shadow to topbar

--- src/components/Topbar/styled.js
+++ src/components/Topbar/styled.js
@@ -9,6 +9,7 @@ export const TopbarWrapper = styled.div`
   width: 100vw;
   color: var(--white);
   padding: .5rem 1rem;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.35);
 ` 
 export const TopbarLinks = styled.div`
   display: flex;
